4bc664de8488e3edd47e683cd30f2f6f2d890c38
tsharptflat <tsharptflat@gmail.com>
2026-04-18 23:53:00 +0900

    Add SteamApiService and UpdateGamePriceJob for fetching game prices

--- app/services/steam_api_service.rb
+++ app/services/steam_api_service.rb
@@ -2,14 +2,14 @@ class SteamApiService
   include HTTParty
   base_uri 'https://store.steampowered.com/api'
 
-  def get_game_price(app_id)
-    response = self.class.get("appdetails", query: {
-      appids: app_id,
+  def get_game_price(steam_app_id)
+    response = self.class.get("/appdetails", query: {
+      appids: steam_app_id,
       cc: 'jp',
       filters: 'price_overview'
     })
 
-    data = response.parsed_response.dig(app_id.to_s, 'data', 'price_overview', 'final')
+    data = response.parsed_response.dig(steam_app_id.to_s, 'data', 'price_overview', 'final')
     return nil unless data
 
     data / 100.0
