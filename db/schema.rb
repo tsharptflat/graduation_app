5f7afd57d9bba6a1ee8e480922c904450c99507e
tsharptflat <tsharptflat@gmail.com>
2026-05-11 00:36:54 +0900

    add:cleared_date column to user_game_libraries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_25_161441) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_10_152647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_25_161441) do
     t.datetime "updated_at", null: false
     t.bigint "game_id", null: false
     t.datetime "last_played_at"
+    t.date "cleared_date"
     t.index ["game_id"], name: "index_user_game_libraries_on_game_id"
     t.index ["user_id", "game_id"], name: "index_user_game_libraries_on_user_id_and_game_id", unique: true
     t.index ["user_id"], name: "index_user_game_libraries_on_user_id"
